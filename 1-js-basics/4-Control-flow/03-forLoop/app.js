26a4e92c4340652a3831b0862f274ece9d08acf4
waweroh <scadmsez@gmail.com>
2024-07-09 23:23:25 +0300

    for loops and nested for loops

---
+++ 1-js-basics/4-Control-flow/03-forLoop/app.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 6; i++) {
+  console.log('outer loop', i);
+  for (let j = 1; j <= 5; j++) {
+    console.log('inner loop', j);
+  }
+}
+// exercise
+// Print your name & numbers from 0 through 1000
+for (let a = 0; a <= 100; a++) {
+  console.log('Moses', a);
+}
