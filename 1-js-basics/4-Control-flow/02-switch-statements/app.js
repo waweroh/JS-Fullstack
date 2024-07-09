d8861384b5146354ec3f7ad849a95bc922324a5f
waweroh <scadmsez@gmail.com>
2024-07-09 23:04:21 +0300

    switch and cases

---
+++ 1-js-basics/4-Control-flow/02-switch-statements/app.js
@@ -0,0 +1,34 @@
+//switch statement
+let day = 'thursday';
+
+switch(day) {
+  case 'monday':
+    console.log('today is monday');
+    break;
+  case 'tuesday':
+    console.log('today is tuesday');
+    break;
+  case 'wednesday':
+    console.log('today is wednesday');
+    break;
+  default:
+    console.log('the week is over as far as Im concerned');
+}
+
+// exercise
+let fruit = 'apple';
+
+switch (fruit) {
+  case 'banana':
+    console.log('banana is good');
+    break;
+  case 'orange':
+    console.log('I am not a fan of orange');
+    break;
+  case 'apple':
+    console.log('how do you like the apples');
+    break;
+  default:
+    console.log('I have never heard of that fruit')
+
+}
