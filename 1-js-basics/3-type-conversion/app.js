f7ca7226b0edcf64b64a90edf8b4e0d7c973d603
waweroh <scadmsez@gmail.com>
2024-07-09 11:45:26 +0300

    add type conversion

---
+++ 1-js-basics/3-type-conversion/app.js
@@ -0,0 +1,16 @@
+let money = 50.456;
+
+//convert string to number
+money = parseInt(money);
+money = Number(money);
+money = +money;
+
+//convert number to string
+money = money.toString();
+money = String(money);
+
+//convert string to decimal
+money = parseFloat(money);
+
+console.log(typeof(money));
+console.log(money);
