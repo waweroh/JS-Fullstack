2bb725be50faba5240ad8ca3542bc3f2748946df
waweroh <scadmsez@gmail.com>
2024-07-10 06:15:00 +0300

    while loop

---
+++ 1-js-basics/4-Control-flow/04-whileLoop/app.js
@@ -0,0 +1,15 @@
+// let i = 1;
+// while (i <= 5){
+//   console.log('hello world', i);
+//   i++; //avoids infinite loop
+// }
+
+// exercise
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10.
+
+let a = 10;
+while (a <= 100) {
+  console.log('Moses', a);
+  a++;
+}
