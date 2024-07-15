95690283f772fbdf9e8d8117e2acd86b7a10a845
waweroh <scadmsez@gmail.com>
2024-07-15 12:41:25 +0300

    renaming in object destructuring

--- 2-intermediate/06-destructuring/02-object-destructuring/app.js
+++ 2-intermediate/06-destructuring/02-object-destructuring/app.js
@@ -18,3 +18,33 @@ const {names, age, country} = person;
 console.log(names);
 console.log(age);
 console.log(country);
+
+
+
+//----------rename destructuring (used in obj only)--------
+const num = {x: 1.0048, y: 48.4983}
+const {x: longitude, y: latitude} = num; //x renamed to longitude, y renamed to latitude
+console.log(longitude);
+console.log(latitude);
+
+//object destructuring and rest operator
+let {a, b, ...rest} = {a: 100, b: 200, c: 500, d: 800};
+console.log(a); // 100
+console.log(b); // 200
+console.log(rest); //{c: 500, d: 800}
+
+//--------exercises--------
+
+// Your task is to use object destructuring to extract the name, age, and country properties from the person object and assign them to separate variables named personName, personAge, and personCountry.
+// After extracting the properties, log each variable's value to the console.
+const person2 = {
+  name: "John Doe",
+  age: 30,
+  gender: "male",
+  country: "USA",
+};
+
+const {name: personName, age: personAge, country:personCountry } = person2
+console.log(personAge)
+console.log(personName)
+console.log(personCountry)
