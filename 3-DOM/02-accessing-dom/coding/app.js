f3141380c30e8125f4ede97d428bef41fbf98749
waweroh <scadmsez@gmail.com>
2024-07-20 11:15:10 +0300

    accessing DOM elements

---
+++ 3-DOM/02-accessing-dom/coding/app.js
@@ -0,0 +1,21 @@
+// ************ DOM SELECTORS ************
+// 1. getElementsByTagName
+console.log(document.getElementsByTagName('h1'))
+// 2. getElementById
+console.log(document.getElementById('main'))
+// 3. getElementsByClassName
+console.log(document.getElementsByClassName('cls'))
+// 4. querySelector
+console.log(document.querySelector('.cls-1'))
+console.log(document.querySelector('#id-1'))
+// 5. querySelectorAll
+console.log(document.querySelectorAll('li'))
+console.log(document.querySelectorAll('li').length)
+
+// ---------------------------------------
+// Storing data in variables
+const h1 = document.querySelector('h1')
+console.log(h1)
+
+
+
